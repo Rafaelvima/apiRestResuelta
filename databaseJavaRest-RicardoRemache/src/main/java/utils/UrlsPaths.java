addc39c24fd9a23bf839b6723f80334034bea25f
Rafaelvima <34725153+Rafaelvima@users.noreply.github.com>
2018-03-03 21:13:08 +0100

    Add files via upload

---
+++ databaseJavaRest-RicardoRemache/src/main/java/utils/UrlsPaths.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author daw
+ */
+public class UrlsPaths {
+
+    public static final String ALUMNOS = "/rest/alumnos";
+    public static final String ASIGNATURAS = "/rest/asignaturas";
+    public static final String NOTAS = "/rest/notas";
+    public static final String APIKEY_FILTRO = "/*";
+    public static final String JSON_FILTRO = "/rest/*";
+    
+    
+
+
+}//fin clase
